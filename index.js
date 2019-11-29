6984a94de7e2013a4f6d2c60fb6a8ae40e6f6a55
Brenden <bhevans17@gmail.com>
2019-11-29 05:10:17 -0800

    Added pages

--- index.js
+++ index.js
@@ -5,4 +5,16 @@ $(document).ready(function() {
     $(".m-side-menu").toggleClass("m-side-menu-show");
     e.preventDefault();
   });
+
+  setInterval(function() {
+    var docHeight = $(window).height();
+    var footerHeight = $("#footer").height();
+    var footerTop = $("#footer").position().top + footerHeight;
+    var marginTop = docHeight - footerTop + 10;
+
+    if (footerTop < docHeight) $("#footer").css("margin-top", marginTop + "px");
+    // padding of 30 on footer
+    else $("#footer").css("margin-top", "0px");
+    // console.log("docheight: " + docHeight + "\n" + "footerheight: " + footerHeight + "\n" + "footertop: " + footerTop + "\n" + "new docheight: " + $(window).height() + "\n" + "margintop: " + marginTop);
+  }, 0);
 });
