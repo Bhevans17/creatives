496b5f729f0dc7908202f20807f6ee1411a2775a
Brenden <bhevans17@gmail.com>
2020-01-14 18:30:45 -0800

    Edited navigation bar

--- includes/header.php
+++ includes/header.php
@@ -32,7 +32,6 @@
                 <li><a href="index.php">HOME</a></li>
                 <li><a href="about.php">ABOUT</a></li>
                 <li><a href="work.php">WORK</a></li>
-                <li><a href="blog.php">BLOG</a></li>
                 <li><a href="contact.php">CONTACT</a></li>
                 <li>
                     <i class="fab fa-facebook"></i>
