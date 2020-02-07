2c16291cc0c0e24a715fb3a7ae91159ba014ed0b
Brenden <bhevans17@gmail.com>
2020-02-07 10:24:35 -0800

    Rewrote entire template

--- js/index.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-  /* Toggle Navigation Menu */
-  $(".fa-bars").on("click", function(e) {
-    $(".fa-bars").toggleClass("fa-times");
-    $(".mobile-side-menu").toggleClass("mobile-side-menu-active");
-    e.preventDefault();
-  });
-});
